ea7001173ecab9bd5205d81ba27431f890333fdc
Nikita Leshenko <nikita@leshenko.net>
2018-12-16 00:48:21 +0200

    write-tree: Hash the files
    Instead of only printing the file name, let's put all files in the object
    database. For now we'll print their OID and their name.
    
    Notice that instead of getting one OID to represent a directory we now get a
    separate OID for each file, which isn't very useful. Plus, note that the names
    of the files aren't stored in the object database, they are just printed and
    then the information is discarded.
    
    So at this stage `write-tree` isn't useful (it just saves a bunch of files as
    blobs) but the next change will fix it.
    
    %% METADATA
    Id: write-tree-hash-files

--- ugit/base.py
+++ ugit/base.py
@@ -11,8 +11,8 @@ def write_tree (directory='.'):
                 continue
 
             if entry.is_file (follow_symlinks=False):
-                # TODO write the file to object store
-                print (full)
+                with open (full, 'rb') as f:
+                    print (data.hash_object (f.read ()), full)
             elif entry.is_dir (follow_symlinks=False):
                 write_tree (full)
 
