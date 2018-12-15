1a2de96ea66b231a5f8fab1be1ae46987cf430e3
Nikita Leshenko <nikita@leshenko.net>
2018-12-16 00:45:17 +0200

    write-tree: Ignore .ugit files
    If we run `ugit write-tree`, we will see that it also prints the content of the
    *.ugit* directory. This directory isn't part of the user's files, so let's
    ignore it.
    
    Actually, I created a separate `is_ignored()` function. This way if we have any
    other files we want to ignore later we have one place to change.
    
    %% METADATA
    Id: write-tree-ignore-ugit

--- ugit/base.py
+++ ugit/base.py
@@ -7,6 +7,9 @@ def write_tree (directory='.'):
     with os.scandir (directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored (full):
+                continue
+
             if entry.is_file (follow_symlinks=False):
                 # TODO write the file to object store
                 print (full)
@@ -14,3 +17,7 @@ def write_tree (directory='.'):
                 write_tree (full)
 
     # TODO actually create the tree object
+
+
+def is_ignored (path):
+    return '.ugit' in path.split ('/')
