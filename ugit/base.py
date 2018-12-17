1f238264303e950dbbe3d9027f96b97ee0110e96
Nikita Leshenko <nikita@leshenko.net>
2018-12-18 00:14:17 +0200

    log: Implement
    `log` will walk the list of commits and print them.
    
    We will start by implementing `get_commit()` that will parse a commit object by
    OID.
    
    Then in the CLI module we will start from the HEAD commit and walk its parents
    until we reach a commit without a parent.
    
    The result is that the entire commit history is printed to the screen once we
    run `ugit log`.
    
    %% METADATA
    Id: log

--- ugit/base.py
+++ ugit/base.py
@@ -1,4 +1,8 @@
+import itertools
+import operator
 import os
+
+from collections import namedtuple
 
 from . import data
 
@@ -92,6 +96,27 @@ def commit (message):
     data.set_HEAD (oid)
 
     return oid
+
+
+Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
+
+
+def get_commit (oid):
+    parent = None
+
+    commit = data.get_object (oid, 'commit').decode ()
+    lines = iter (commit.splitlines ())
+    for line in itertools.takewhile (operator.truth, lines):
+        key, value = line.split (' ', 1)
+        if key == 'tree':
+            tree = value
+        elif key == 'parent':
+            parent = value
+        else:
+            assert False, f'Unknown field {key}'
+
+    message = '\n'.join (lines)
+    return Commit (tree=tree, parent=parent, message=message)
 
 
 def is_ignored (path):
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import textwrap
 
 from . import base
 from . import data
@@ -39,6 +40,9 @@ def parse_args ():
     commit_parser.set_defaults (func=commit)
     commit_parser.add_argument ('-m', '--message', required=True)
 
+    log_parser = commands.add_parser ('log')
+    log_parser.set_defaults (func=log)
+
     return parser.parse_args ()
 
 
@@ -67,3 +71,15 @@ def read_tree (args):
 
 def commit (args):
     print (base.commit (args.message))
+
+
+def log (args):
+    oid = data.get_HEAD ()
+    while oid:
+        commit = base.get_commit (oid)
+
+        print (f'commit {oid}\n')
+        print (textwrap.indent (commit.message, '    '))
+        print ('')
+
+        oid = commit.parent
