1599f6aa74d5540cfeba36fb8403c54d39d208dc
Nikita Leshenko <nikita@leshenko.net>
2019-01-29 00:04:50 +0200

    data: Create RefValue container
    To make working with symbolic refs easier, we will create a `RefValue` container
    to represent the value of a ref. `RefValue` will have a property `symbolic` that
    will say whether it's a symbolic or a direct ref.
    
    This change is just refactoring, we will wrap every OID that is written or read
    from a ref in a `RefValue`.
    
    %% METADATA
    Id: create-refvalue

--- ugit/base.py
+++ ugit/base.py
@@ -85,7 +85,7 @@ def read_tree (tree_oid):
 def commit (message):
     commit = f'tree {write_tree ()}\n'
 
-    HEAD = data.get_ref ('HEAD')
+    HEAD = data.get_ref ('HEAD').value
     if HEAD:
         commit += f'parent {HEAD}\n'
 
@@ -94,7 +94,7 @@ def commit (message):
 
     oid = data.hash_object (commit.encode (), 'commit')
 
-    data.update_ref ('HEAD', oid)
+    data.update_ref ('HEAD', data.RefValue (symbolic=False, value=oid))
 
     return oid
 
@@ -102,15 +102,15 @@ def commit (message):
 def checkout (oid):
     commit = get_commit (oid)
     read_tree (commit.tree)
-    data.update_ref ('HEAD', oid)
+    data.update_ref ('HEAD', data.RefValue (symbolic=False, value=oid))
 
 
 def create_tag (name, oid):
-    data.update_ref (f'refs/tags/{name}', oid)
+    data.update_ref (f'refs/tags/{name}', data.RefValue (symbolic=False, value=oid))
 
 
 def create_branch (name, oid):
-    data.update_ref (f'refs/heads/{name}', oid)
+    data.update_ref (f'refs/heads/{name}', data.RefValue (symbolic=False, value=oid))
 
 
 Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
@@ -161,8 +161,8 @@ def get_oid (name):
         f'refs/heads/{name}',
     ]
     for ref in refs_to_try:
-        if data.get_ref (ref):
-            return data.get_ref (ref)
+        if data.get_ref (ref).value:
+            return data.get_ref (ref).value
 
     # Name is SHA1
     is_hex = all (c in string.hexdigits for c in name)
