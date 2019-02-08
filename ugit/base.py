4a5b0f2b0153dafd85c90c39ada121558b27cbd4
Nikita Leshenko <nikita@leshenko.net>
2019-02-09 00:35:35 +0200

    read-tree: Delete all existing stuff before reading
    This is done so that we won't have any old files left around after a read-tree.
    
    Before this change, if we save tree A which contains only *a.txt*, then we save
    tree B which contains *a.txt* and *b.txt* and then we `read-tree` A, we will have
    *b.txt* left over in the working directory.
    
    %% METADATA
    Id: read-tree-cleanup

--- ugit/base.py
+++ ugit/base.py
@@ -50,7 +50,27 @@ def get_tree (oid, base_path=''):
     return result
 
 
+def _empty_current_directory ():
+    for root, dirnames, filenames in os.walk ('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath (f'{root}/{filename}')
+            if is_ignored (path) or not os.path.isfile (path):
+                continue
+            os.remove (path)
+        for dirname in dirnames:
+            path = os.path.relpath (f'{root}/{dirname}')
+            if is_ignored (path):
+                continue
+            try:
+                os.rmdir (path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory contains ignored files,
+                # so it's OK
+                pass
+
+
 def read_tree (tree_oid):
+    _empty_current_directory ()
     for path, oid in get_tree (tree_oid, base_path='./').items ():
         os.makedirs (os.path.dirname (path), exist_ok=True)
         with open (path, 'wb') as f:
