453463b5bbfb0f4505eeab35ae70d62eb17da47d
Nikita Leshenko <nikita@leshenko.net>
2019-08-11 00:41:04 +0300

    data: Allow git directory change
    We're going to implement repository syncronization soon (`fetch`, `push`). In
    preparation for that, we need to allow ugit to change the current `GIT_DIR`, so
    that we can temporarily look at different repositories while syncronizing.
    
    Let's use `contextmanager` to allow the change to be performed in a `with`
    statement so that directory changes are easily stackable and revertible.
    
    If you're unfamiliar with the `contextmanager` decorator,
    <a href="https://book.pythontips.com/en/latest/context_managers.html" target="_blank">see this
    tutorial</a>.
    
    %% METADATA
    Id: change-git-directory

--- ugit/data.py
+++ ugit/data.py
@@ -2,8 +2,20 @@ import hashlib
 import os
 
 from collections import namedtuple
+from contextlib import contextmanager
 
-GIT_DIR = '.ugit'
+
+# Will be initialized in cli.main()
+GIT_DIR = None
+
+
+@contextmanager
+def change_git_dir (new_dir):
+    global GIT_DIR
+    old_dir = GIT_DIR
+    GIT_DIR = f'{new_dir}/.ugit'
+    yield
+    GIT_DIR = old_dir
 
 
 def init ():
