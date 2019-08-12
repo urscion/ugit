5127966fbec447a2259aaa20895048afd78297a3
Nikita Leshenko <nikita@leshenko.net>
2019-08-12 21:37:32 +0300

    status: Show staged and non-staged modified files
    To make user's life easier, let's extend the `status` command to display which
    changed files will be committed and which files were changed but not going to be
    committed.
    
    The change is quite simple because it relies on existing infrastructure. To
    display changed files that **are** going to be committed, we will compare index
    to HEAD. To display changed files that **aren't** going to be committed we will
    compare the index to the working tree.
    
    Note that a file can be present in both groups - maybe we changed it, added it
    to the index, changed it some more but didn't add the latest changes to the
    index.
    
    %% METADATA
    Id: status-show-staged

--- ugit/cli.py
+++ ugit/cli.py
@@ -233,6 +233,11 @@ def status (args):
     print ('\nChanges to be committed:\n')
     HEAD_tree = HEAD and base.get_commit (HEAD).tree
     for path, action in diff.iter_changed_files (base.get_tree (HEAD_tree),
+                                                 base.get_index_tree ()):
+        print (f'{action:>12}: {path}')
+
+    print ('\nChanges not staged for commit:\n')
+    for path, action in diff.iter_changed_files (base.get_index_tree (),
                                                  base.get_working_tree ()):
         print (f'{action:>12}: {path}')
 
