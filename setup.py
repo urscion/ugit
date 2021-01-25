e2fb3b03709ae52c092101db152d58872485bb16
Peter Clay <pwclay@gmail.com>
2021-01-25 18:21:23 -0500

    Implement the Object Database

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup
 
-setup (name = 'ugit',
-       version = '1.0',
-       packages = ['ugit'],
-       entry_points = {
-           'console_scripts' : [
-               'ugit = ugit.cli:main'
-           ]
-       })
+setup(name='ugit',
+      version='1.0',
+      packages=['ugit'],
+      entry_points={
+        'console_scripts': [
+            'ugit = ugit.cli:main'
+         ]
+      })
